16a72abc5d6700961b3b958607b9fd8078164214
shahvezjumani <sha685789@gmail.com>
2023-11-14 22:44:52 +0500

    display methods were added

--- Tree.java
+++ Tree.java
@@ -1,4 +1,7 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Tree {
     Node root;
@@ -27,6 +30,80 @@ public class Tree {
             int value = scanner.nextInt();
             node.right = new Node(value);
             insert(scanner,node.right);
+        }
+    }
+    public void displayPreOrder(){
+        if(root == null){
+            return;
+        }
+        displayPreOrder(root);
+    }
+    public void displayPreOrder(Node node){
+        if(node == null){
+            return;
+        }
+        System.out.print(node.data+" ");
+        displayPreOrder(node.left);
+        displayPreOrder(node.right);
+    }
+
+    public void displayPostOrder(){
+        if(root == null){
+            return;
+        }
+        displayPreOrder(root);
+    }
+    public void displayPostOrder(Node node){
+        if(node == null){
+            return;
+        }
+        displayPreOrder(node.left);
+        displayPreOrder(node.right);
+        System.out.print(node.data+" ");
+    }
+
+    public void displayInOrder(){
+        if(root == null){
+            return;
+        }
+        displayPreOrder(root);
+    }
+    public void displayInOrder(Node node){
+        if(node == null){
+            return;
+        }
+        displayPreOrder(node.left);
+        System.out.print(node.data+" ");
+        displayPreOrder(node.right);
+    }
+
+    public void displayLevelTraversal(){
+        if(root == null){
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        while(!q.isEmpty()){
+            Node curr = q.remove();
+            if(curr == null){
+                System.out.println();
+                if(q.isEmpty()){
+                    break;
+                }
+                else {
+                    q.add(null);
+                }
+            }
+            else {
+                System.out.print(curr.data+" ");
+                if(curr.left != null){
+                    q.add(curr.left);
+                }
+                if(curr.right != null) {
+                    q.add(curr.right);
+                }
+            }
         }
     }
 }
